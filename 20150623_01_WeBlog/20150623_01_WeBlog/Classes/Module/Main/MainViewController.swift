57fbe5110ba74becbf17a7c80f0274e2cfb2f56d
BeNotJustCoder <benotjustcoder@163.com>
2015-06-23 21:11:51 +0800

    1、各个模块的目录搭建。

---
+++ 20150623_01_WeBlog/20150623_01_WeBlog/Classes/Module/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  20150623_01_WeBlog
+//
+//  Created by lincy on 15/6/23.
+//  Copyright © 2015年 lincy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
